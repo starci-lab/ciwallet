fcbdfccbdfd1fd1ecc5ef4a67fb19ac53ed9d3d9
starci183 <cuongnvtse160875@gmail.com>
2024-08-31 02:02:47 +0700

    123

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -13,13 +13,21 @@ const Page = () => {
             <div className="w-full">
                 <div className="grid place-items-center gap-4 w-full">
                     <Image removeWrapper src="/logo.svg" height={150} />
-                </div>  
-                <Spacer y={12}/>
+                </div>
+                <Spacer y={12} />
                 <div className="grid gap-4 w-full">
-                    <Button color="primary" onPress={() => router.push(constantConfig().path.create)}>
-        Create
+                    <Button
+                        variant="flat"
+                        onPress={() => router.push(constantConfig().path.create)}
+                    >
+            Create
                     </Button>
-                    <Button color="primary">Import</Button>
+                    <Button
+                        variant="flat"
+                        onPress={() => router.push(constantConfig().path.mnemonicInput)}
+                    >
+            Import
+                    </Button>
                 </div>
             </div>
         </Container>
