fc4fc6f77681788ded9d6eef7661e79aa3906d9e
starci183 <cuongnvtse160875@gmail.com>
2024-09-12 01:28:09 +0700

    123123

--- src/app/cifarm/UnityCanvas/useUnity.ts
+++ src/app/cifarm/UnityCanvas/useUnity.ts
@@ -2,20 +2,18 @@ import { use } from "react"
 import { useUnityContext } from "react-unity-webgl"
 import { UnityContextHook } from "react-unity-webgl/distribution/types/unity-context-hook"
 import { HooksContext } from "./provider.hooks"
+import { envConfig } from "@/config"
 
 export interface UseUnityReturn {
     unity: UnityContextHook
 }
 
-const FILE_NAME = "cifarm-build"
-const BUILD_PATH = "games/cifarm-build/Build"
-
 export const _useUnity = () : UseUnityReturn => {
     const unity = useUnityContext({
-        loaderUrl: `${BUILD_PATH}/${FILE_NAME}.loader.js`,
-        dataUrl: `${BUILD_PATH}/${FILE_NAME}.data.unityweb`,
-        frameworkUrl: `${BUILD_PATH}/${FILE_NAME}.framework.js.unityweb`,
-        codeUrl: `${BUILD_PATH}/${FILE_NAME}.wasm.unityweb`,
+        loaderUrl: envConfig().externals.cifarm.packages.loaderUrl,
+        dataUrl: envConfig().externals.cifarm.packages.dataUrl,
+        frameworkUrl: envConfig().externals.cifarm.packages.frameworkUrl,
+        codeUrl: envConfig().externals.cifarm.packages.wasmUrl,
     })
     
     return {
--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -11,6 +11,12 @@ export const envConfig = () => ({
                 api: process.env.NEXT_PUBLIC_CIFARM_PERIPHERY_API_URL,
                 graphql: process.env.NEXT_PUBLIC_CIFARM_PERIPHERY_GRAPHQL_URL,
             },
+            packages: {
+                loaderUrl: process.env.NEXT_PUBLIC_CIFARM_LOADER_URL ?? "",
+                dataUrl: process.env.NEXT_PUBLIC_CIFARM_DATA_URL ?? "",
+                frameworkUrl: process.env.NEXT_PUBLIC_CIFARM_FRAMEWORK_URL ?? "",
+                wasmUrl: process.env.NEXT_PUBLIC_CIFARM_WASM_URL ?? ""
+            }
         },
     },
     appSalt: process.env.APP_SALT,
