88ac3d1d14b8b6eb9bb2342eba70396c3d063a35
starci183 <cuongnvtse160875@gmail.com>
2024-10-01 22:53:24 +0700

    123123

--- src/app/home/NFTs/index.tsx
+++ src/app/home/NFTs/index.tsx
@@ -28,7 +28,7 @@ export const NFTs = () => {
                 const keys = Object.keys(nfts)
                 return (
                     <Card key={groupKey}>
-                        <CardBody className="px-3 py-2.5">
+                        <CardBody className="p-3">
                             <div className="flex gap-2 items-center">
                                 <Image
                                     removeWrapper
