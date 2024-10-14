60db510404e9d16b0cd32c0a04e5b293d331b7ab
starci183 <cuongnvtse160875@gmail.com>
2024-10-14 16:33:39 +0700

    123123

--- src/hooks/miscellaneous/useRouterWithSearchParams.ts
+++ src/hooks/miscellaneous/useRouterWithSearchParams.ts
@@ -3,7 +3,7 @@ import { useRouter, useSearchParams } from "next/navigation"
 export const useRouterWithSearchParams = () => {
     const router = useRouter()
     const searchParams = useSearchParams()
-    const empty = searchParams.keys().toArray().length
+    const empty = Array(searchParams.keys()).length
     const push = (href: string) =>
         empty ? router.push(`${href}?${searchParams.toString()}`) : router.push(href)
 
