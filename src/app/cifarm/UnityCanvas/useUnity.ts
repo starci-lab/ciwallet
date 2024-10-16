4aaf99f7a36245cfdba9d58041259a0e500248ed
starci183 <cuongnvtse160875@gmail.com>
2024-10-16 16:36:10 +0700

    123123

--- src/app/cifarm/UnityCanvas/useUnity.ts
+++ src/app/cifarm/UnityCanvas/useUnity.ts
@@ -9,7 +9,7 @@ export interface UseUnityReturn {
   unity: UnityContextHook;
 }
 
-export const VERSION = "v.1.2.a"
+export const VERSION = "v.1.3.a"
 
 const gameUrl = (name: string) => {
     const isDev = envConfig().isDev
